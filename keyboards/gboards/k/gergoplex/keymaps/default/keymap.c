dd24a393deb7a9ccb2af7cbf0f62541b47fc7d4d
denis <ki7dk@lima7.com>
2021-05-26 11:09:45 -0700

    spacing

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -81,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          '-----------------'           '-----------------' 
  */
 [NUMB] = LAYOUT_gergoplex(
-    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,
+    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                 KC_6,    KC_7,    KC_8,     KC_9,    KC_0,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                   KC_TRNS,KC_TRNS,KC_TRNS,    KC_TRNS,KC_TRNS,KC_MNXT
+                   KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,KC_TRNS,KC_MNXT
     )
 //[NUMB] = LAYOUT_gergoplex(
  //   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,
