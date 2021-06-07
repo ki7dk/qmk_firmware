43a60128f34ec916f71ff72251701fe0ececbc2a
denis <ki7dk@lima7.com>
2021-06-07 10:07:36 -0700

    changes

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -16,7 +16,7 @@
 /* Combomap
  *
  * ,------------------------------.      ,--------------------------------.
- * |      |    ESC    |     |     |      |     |    ESC    |    BSLH      |
+ * | TAB  |    ESC    |     |     |      |     |    ESC    |    BSLH      |
  * |-----+-----+-----+-----+------|      |--------------------------------|
  * |      |   BSPC   ENT    |     |      |    LES   COLN  GRT    |        | 
  * |-----+-----+-----+--RMB+-LMB--+      |--------------------------------|
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,    MT(MOD_LCTL, KC_SCLN),
     MT(MOD_RSFT, KC_Z),KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
 
-     MT(MOD_LGUI, KC_ESC), MT(MOD_LALT, KC_BSPC), LT(SYMB, KC_BSPC),                  // Left
+     MT(MOD_LALT, KC_ESC), MT(MOD_LGUI, KC_BSPC), LT(SYMB, KC_BSPC),                  // Left
     LT(NUMB, KC_ENT), MT(KC_LSFT, KC_SPC) , MT(MOD_RSFT, KC_TAB)                                // Right
     ),
 /* Keymap 1: Symbols layer
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                 KC_6,    KC_7,    KC_8,     KC_9,    KC_0,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                   KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,KC_TRNS,KC_MNXT
+                   KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,KC_TRNS,KC_TRNS
     )
 //[NUMB] = LAYOUT_gergoplex(
  //   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,
